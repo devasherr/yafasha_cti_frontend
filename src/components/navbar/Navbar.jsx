7bdfa449de734fbac62e9b4d7df8ce33c39128e1
Ashermuzic <ashersam116@gmail.com>
2024-06-24 00:28:45 -0900

    styling for sentiment page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,6 @@
 import "./navbar.scss";
-import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
-import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
-import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 import { DarkModeContext } from "../../context/darkModeContext";
 import { useContext } from "react";
 
@@ -15,15 +10,8 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="wrapper">
-        <div className="search">
-          <input type="text" placeholder="Search..." />
-          <SearchOutlinedIcon />
-        </div>
+        <div className="search"></div>
         <div className="items">
-          <div className="item">
-            <LanguageOutlinedIcon className="icon" />
-            English
-          </div>
           <div className="item">
             <DarkModeOutlinedIcon
               className="icon"
@@ -31,18 +19,8 @@ const Navbar = () => {
             />
           </div>
           <div className="item">
-            <FullscreenExitOutlinedIcon className="icon" />
-          </div>
-          <div className="item">
             <NotificationsNoneOutlinedIcon className="icon" />
             <div className="counter">1</div>
-          </div>
-          <div className="item">
-            <ChatBubbleOutlineOutlinedIcon className="icon" />
-            <div className="counter">2</div>
-          </div>
-          <div className="item">
-            <ListOutlinedIcon className="icon" />
           </div>
           <div className="item">
             <img
--- src/pages/sentiment/Sentiment.jsx
+++ src/pages/sentiment/Sentiment.jsx
@@ -1,37 +1,50 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./sentiment.scss";
 
 const Sentiment = () => {
   const [sentimentResult, setSentimentResult] = useState([]);
   const [value, setValue] = useState({
     text: "",
   });
+  const [loading, setLoading] = useState(false);
 
   const getSentimentAnalysis = () => {
+    setLoading(true);
     axios
       .post("http://localhost:5000/sentiment", {
         text: value.text,
       })
       .then((res) => {
         setSentimentResult(res.data);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
       });
   };
 
   return (
     <>
-      <div>
-        <input
-          type="text"
-          placeholder="enter here"
-          value={value.text}
-          onChange={(e) => setValue({ text: e.target.value })}
-        ></input>
+      <div className="sentiment-container">
+        <div className="sentiment-title">Sentiment analysis</div>
+        <div className="sentiment-form">
+          <textarea
+            type="text"
+            placeholder="enter here"
+            value={value.text}
+            onChange={(e) => setValue({ text: e.target.value })}
+          ></textarea>
 
-        <button onClick={getSentimentAnalysis}>process</button>
-        <p>{sentimentResult}</p>
+          <button onClick={getSentimentAnalysis}>process</button>
+        </div>
+
+        {loading ? (
+          <div className="loader"></div>
+        ) : (
+          <p className={sentimentResult}>{sentimentResult}</p>
+        )}
       </div>
     </>
   );
