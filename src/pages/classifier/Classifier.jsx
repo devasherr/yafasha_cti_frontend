91e2720f5f55d3308f3b40b3861c25e244c38427
Ashermuzic <ashersam116@gmail.com>
2024-06-24 00:37:06 -0900

    styling for classifier

--- src/pages/classifier/Classifier.jsx
+++ src/pages/classifier/Classifier.jsx
@@ -1,37 +1,46 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./classifier.scss";
 
 const Classifier = () => {
   const [classifierResult, setClassifierResult] = useState([]);
   const [value, setValue] = useState({
     text: "",
   });
+  const [loading, setLoading] = useState(false);
 
-  const getClassifierAnalysis = () => {
+  const getAttackClassifier = () => {
+    setLoading(true);
     axios
       .post("http://localhost:5000/classifier", {
         text: value.text,
       })
       .then((res) => {
         setClassifierResult(res.data);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
       });
   };
 
   return (
     <>
-      <div>
-        <input
-          type="text"
-          placeholder="enter here"
-          value={value.text}
-          onChange={(e) => setValue({ text: e.target.value })}
-        ></input>
+      <div className="classifier-container">
+        <div className="classifier-title">Attack Classifier</div>
+        <div className="classifier-form">
+          <textarea
+            type="text"
+            placeholder="enter here"
+            value={value.text}
+            onChange={(e) => setValue({ text: e.target.value })}
+          ></textarea>
 
-        <button onClick={getClassifierAnalysis}>process</button>
-        <p>{classifierResult}</p>
+          <button onClick={getAttackClassifier}>process</button>
+        </div>
+
+        {loading ? <div className="loader"></div> : <p>{classifierResult}</p>}
       </div>
     </>
   );
